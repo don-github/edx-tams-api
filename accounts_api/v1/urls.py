048f77a09257a51ade64b9d99441a9a5d18c4d3f
kencung <ken@rapidthunder.us>
2015-10-01 22:38:03 -0400

    fixed url for get request

--- accounts_api/v1/urls.py
+++ accounts_api/v1/urls.py
@@ -6,11 +6,10 @@ from django.conf.urls import patterns, url
 
 from . import views
 
+USERNAME_PATTERN = '(?P<username>[\w.@+-]+)'
+
 urlpatterns = patterns(  # pylint: disable=invalid-name
     '',
-    url(
-        r'^accounts$',
-        views.AccountsView.as_view(),
-        name='accounts_api'
-    )
+    url(r'^accounts/{username}$'.format(username=USERNAME_PATTERN), views.AccountsView.as_view(), name='accounts_api'),
+    url(r'^accounts$', views.AccountsView.as_view(), name='accounts_api'),
 )
