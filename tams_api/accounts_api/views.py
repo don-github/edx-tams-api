62003fe9a0641e0768109b8941443ab871b08648
kencung <ken@rapidthunder.us>
2015-10-07 14:44:23 -0400

    Fixed bug getting course id after enrolling

--- tams_api/accounts_api/views.py
+++ tams_api/accounts_api/views.py
@@ -129,7 +129,7 @@ class AccountsView(APIView):
                 log.info("Enrolling course {} for user {}".format(course_id, username))
 
                 enrollment = create_user_enrollment(username, course_id)
-                user['enrolled_course_id'] = enrollment['course']['course_id']
+                user['enrolledCourseId'] = enrollment['course_details']['course_id']
             except Exception as err:
                 log.error(err.message)
 
